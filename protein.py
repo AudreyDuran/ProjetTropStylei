fa2073a2bcc73f5f31a0a296734e74596ccf7c71
ArthurPERE <arthur.pere@insa-lyon.fr>
2015-05-21 12:03:46 +0200

    marche avec un essai

--- protein.py
+++ protein.py
@@ -1,23 +1,15 @@
 import numpy
 import math
 import random
-from envir import *
 
-envire = envir(20,500)
+import os
 
 class protein:
-	def __init__(self, rayon, x = (envire.fin - envire.debut)*random.random() - envire.fin, y = envire.diametre*random.random()):
+	def __init__(self, rayon, x, y):
 		
 		self.x = x
 		self.y = y
 		self.rayon = rayon
-
-		self.diametre = envire.diametre
-		self.position_trou = envire.position_trou
-		self.taille_trou = envire.position_trou
-
-		self.debut = envire.debut
-		self.fin = envire.fin
 
 	def detection(self, a):
 
@@ -30,23 +22,48 @@ class protein:
 			return False
 
 
-	def move(self, dt, vitesse_lim):
+	def move(self, dt, vitesse_lim, position_trou, taille_trou, debut, fin, diametre):
 
-		self.x += dt * vitesse_lim * ( 5/4 * random.random() - 1 ) # 5/4 pour dire que la proteine peut revenir
-		self.y += dt * vitesse_lim * ( 2 * random.random() - 1 ) # 2 pour que la proteine monte ou desend
+		self.x += dt * random.uniform(-1/16*vitesse_lim, vitesse_lim)
+		self.y += dt * random.uniform(-vitesse_lim, vitesse_lim)
 
 		if self.y < 0 :
 			self.y = 0
 
-		if self.y > self.diametre & (self.x < self.position_trou | self.x > (self.position_trou+self.taille_trou) : 
-			self.y = self.diametre
+		if self.x < 0:
+			self.x = 0
 
-		if self.x > self.fin :
-			self.x = self.debut
-			self.y = self.diametre*random.random()
+		if self.y > diametre:
+			if self.x < position_trou :
+				if self.x > (position_trou+taille_trou) : 
+					self.y = diametre
+
+		if self.x > fin :
+			self.x = debut
+			self.y = random.uniform(0, diametre)
 
 
-p = protein(200)
-a = protein(400)
 
-print p.x
+
+
+
+
+p = protein(20, random.random(), random.random())
+a = protein(40, random.random(), random.random())
+
+
+f = open("position.txt", "w")
+for t in xrange(5000):
+	p.move(0.1, 20, 10, 60, 0, 100, 100)
+	a.move(0.1, 20, 10, 60, 0, 100, 100)
+
+	f.write("%f %f %f %f\n" %(p.x, p.y, a.x, a.y))
+
+f.close()
+
+f = open("fichiergnuplot","w")
+f.write('plot \"position.txt\" using 1:2 \n')
+f.write('replot \"position.txt\" using 3:4\n')
+
+f.close()
+os.system("gnuplot fichiergnuplot --persist")
