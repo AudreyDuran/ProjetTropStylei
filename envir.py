fb1d07baea0fc7f341de4e39819379554bbdda8e
Duran Audrey <aduran@bs406-s16-25.insa-lyon.fr>
2015-05-21 11:13:10 +0200

    Commentaires classe envir + enlever fonction/attributs

--- envir.py
+++ envir.py
@@ -3,27 +3,22 @@ import math
 import random
 
 class envir:
-    def __init__(self, taille_trou, diametre, position_trou, debut, fin):
-    	self.taille_trou = taille_trou
-    	self.diametre = diametre
 
-    	self.debut = debut
+    #taille trou : longueur du trou
+    #position_trou: position x du trou (par rapport au debut du vaisseau)
+    #diametre : diametre du vaisseau (longueur en y)
+    #debut: indice x de debut du vaisseau (par rapport a la fenetre)
+    #fin: indice x de fin du vaisseau (y-x=longueur du vaisseau)
+
+    def __init__(self, taille_trou, position_trou, diametre, debut, fin):
+    	self.taille_trou = taille_trou
+        self.position_trou = position_trou
+
+    	self.diametre = diametre
+        self.debut=debut
     	self.fin = fin
 
-    	self.position_trou = position_trou
-
-    	self.paroi_basse = [[diametre, debut], [diametre, position_trou], [diametre, position_trou+taille_trou], [diametre, fin]]
-
-    	self.trou = [[diametre,position_trou], [diametre,position_trou+taille_trou]]
-
     	self.compteur = 0
-
-    
-
-    def update_paroi(self, x_plaquette, y_plaquette, taille_plaquette):
-    	self.paroi_basse.append([[x_plaquette,y_plaquette], [x_plaquette,y_plaquette+taille_plaquette]])
-
-    	self.compteur += 1
 
 
 a = envir(50,500)
