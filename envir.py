bed4ea59d559474de181c67ce2012aefe29c83cd
Duran Audrey <aduran@bs406-s16-25.insa-lyon.fr>
2015-05-21 16:17:32 +0200

    initiation envir.py dico fini

--- envir.py
+++ envir.py
@@ -11,6 +11,7 @@ class envir:
     #fin: indice x de fin du vaisseau (y-x=longueur du vaisseau)
     #protPermanentes : liste des prot seront dans le sang a chaque essai et seront affichees
     #protTotales : liste de ttes  les proteines dans le sang
+    #dicoRel : contient pour chaque prot ( (affinite,devient),(inhibe) )
 
     def __init__(self, taille_trou, position_trou, diametre, debut, fin):
     	self.taille_trou = taille_trou
@@ -24,13 +25,27 @@ class envir:
 
         self.protPermanentes=[]
         self.protTotales=[]
+
         self.dicoRel={}
 
-        self.dicoRel['fVIIa']=('TF')
-        self.dicoRel['TF']=('fVIIa')
-        self.dicoRel['X']=('VIIa-TF')
+        self.dicoRel['fVIIa']=('TF','VIIa-TF')
+        self.dicoRel['TF']=('fVIIa','VIIa-TF')
 
+        self.dicoRel['X']=('VIIa-TF','Xa')
+        self.dicoRel['VIIa-TF']=('X','Xa')
 
+        self.dicoRel['prothrombine']=('Xa','thrombine')
+        self.dicoRel['Xa']=(('prothrombine','thrombine'),('V','Va'))
+        self.dicoRel['V']=('Xa','Va')
+
+        self.dicoRel['fibrinogene']=('thrombine','fibrine')
+        self.dicoRel['thrombine']=('fibrinogene','fibrine')
+
+        #on le met vraiment le cross linked fibrin clot ?
+        self.dicoRel['XIIIa']=('fibrine','fibrinclot')
+        self.dicoRel['fibrine']=('XIIIa','fibrinclot')
+
+        self.venin=""
 
 
 
@@ -40,3 +55,6 @@ class envir:
 
 
 
+e=envir(20,40,80,0,100)
+#e.dicoRel['Xa'][0]=0
+print e.dicoRel['Xa'][1]
