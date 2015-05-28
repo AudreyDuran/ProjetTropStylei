c9082606e998e18f71958ac4399ceb448ec0e900
Your Name <you@example.com>
2015-05-28 09:11:01 +0200

    commentaires pour changer structure environnement

--- envir.py
+++ envir.py
@@ -26,6 +26,15 @@ class envir:
         self.protPermanentes=[]
 
         self.protTotales=[]
+        self.fVIIa=[]
+        self.TF=[]
+        self.X=[]
+        self.VIIa-TF=[]
+        self.prothrombine=[]
+        self.Xa=[]
+        self.V=[]
+        self.fibrinogene=[]
+        self.thrombine=[]
 
         self.dicoRel={}
 
@@ -49,6 +58,16 @@ class envir:
         self.dicoRel['fibrine']=('XIIIa','fibrinclot')
 
         #METTRE LE NOM DES LISTES A LA PLACE DES NOMS DE PROT DANS LE DICO
+        #pas possible, cles peuvent pas etre des types listes.. types prot ?
+
+        #pb=besoin des prot pour appeler les fonctions, donc dans le dico je dois avoir des prot... 
+        #ou un truc7
+
+        #solution = un dico ou un type de facteur en cle et toutes les prot en valeur
+        #du coup : plus besoin de liste des prot totales, on parcourt tout le dico
+        #pr chaque prot d'un type, on fait dicoRel[cle][1] detection
+
+        #et si cle=string et valeurs = les listes? comment on a la string ?
 
         self.venin=""
         self.vitesse_lim=20
@@ -56,17 +75,19 @@ class envir:
 
     #calcule la nouvelle position de toutes les proteines
     def moveAll(self):
-        for l in self.protTotales: #on parcourt chaque liste de prot
-            for i in xrange(len(l)): #on regarde chaque prot de cette liste
+        for l in self.protTotales: #on parcourt chaque liste de prot (l=liste d'un type de prot)
+            for i in xrange(len(l)): #on regarde chaque prot de cette liste  (l[i]=une prot de cette liste d'un type)
 
-                if len(self.dicoRel[l[i][0]])==1: #si peut reagir qu'avec une proteine
+                if l[i].activation==False: #si est pas deja activee (auquel cas ne peut pas bouger)
 
-                    if l[i].detection(self.dicoRel[l[i][0]])== False:  #si detecte pas de la prot avec qui peut reagir
-                        l[i].move(self.compteur, self.vitesse_lim, self.position_trou, self.taille_trou, self.debut, self.fin, self.diametre)
+                    if len(self.dicoRel[l[i][0]])==1: #si peut reagir qu'avec un type de proteine
 
-                    else: #si detecte une prot avec qui reagit
-                        l[i].activation=True
-                        #comment activer la prot aussi
+                        #faire une boucle for chaque prot de la liste des prot avec qui reagit
+                        if l[i].detection(self.dicoRel[l[i][0]])== False:  #si detecte pas une des prot avec qui peut reagir
+                            l[i].move(self.compteur, self.vitesse_lim, self.position_trou, self.taille_trou, self.debut, self.fin, self.diametre)
+
+                        else: #si detecte une prot avec qui reagit
+                            l[i].activation=True
 
 
 
