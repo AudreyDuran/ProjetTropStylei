97a856851f7b10b6b8793d96fd9b06925210d522
ArthurPERE <arthur.pere@insa-lyon.fr>
2015-05-28 11:48:31 +0200

    ne marche pas

--- interface.py
+++ interface.py
@@ -11,18 +11,47 @@ class MyFrame(wx.Frame):
 
 
 
-        # Check box
-        self.cb = wx.CheckBox(self,-1, 'venin', (200,200))
-        self.Bind(wx.EVT_CHECKBOX, self.imprimer , self.cb)
+        self.cb = wx.CheckBox(self,-1,'venin',(200,200))
+        self.Bind(wx.EVT_CHECKBOX, self.imprimer_b1 , self.cb)
         
-        # # Text
-        # tx = wx.TextCtrl( self, -1, 'text', (100,0))
-        # self.Bind(wx.EVT_CHECKBOX, self.imprimer_b1 , self.cb)
+        tx = wx.TextCtrl( self, -1, 'text', (100,0))
+        self.Bind(wx.EVT_CHECKBOX, self.imprimer_b1 , self.cb)
 
         self.boutton = []
-        self.texte = []
         self.compteur = 0
         self.compteur1 = 0
+        
+        
+        
+    def bouton(self, label):
+        for i,v in enumerate(label):
+        	b = wx.Button(self, i, (0, i*35), (80, 33))
+        	self.boutton.append(b)
+
+
+
+    def bind_bouton(self, fonction):
+    	for i,v in enumerate(self.boutton):
+    		self.Bind(wx.EVT_BUTTON, fonction[i], v)
+
+
+    def imprimer_b0(self,event):
+    	print self.boutton[0].GetLabel()
+
+    def imprimer_b1(self,event):
+    	print self.boutton[1].GetLabel()
+    	self.Destroy()
+
+    def imprimer_b2(self,event):
+    	self.compteur += 1
+    	print self.compteur
+
+    def imprimer_b3(self,event):
+    	self.compteur1 += 1
+    	print self.boutton[3].GetLabel()
+    	if self.compteur1 >= 5:
+    		self.Destroy()
+
 
 
 
@@ -64,17 +93,15 @@ class MyApp(wx.App):
 		frame = MyFrame(None, -1, "coagulation",(40,40),(400,400))
 
 		label = ['1', '2', '3', '4']
-		bind = [frame.imprimer for i in xrange(1,4)]
-
-
+	
         # bouton
 		frame.bouton(label)
-		frame.bind_bouton(bind)
+		frame.bind_bouton([frame.imprimer_b0,frame.imprimer_b1,frame.imprimer_b2,frame.imprimer_b3])
 
 
         # texte
 		frame.text(label)
-		frame.bind_texte(bind)
+		# frame.bind_texte(bind)
 
 		frame.Show(True)
 		self.SetTopWindow(frame)
