83264718d8e065c5864f21b4c55b468e40079dc1
Your Name <you@example.com>
2015-05-30 18:41:36 +0200

    fin methode reaction + avancement methode moveAll (prend methode reaction en compte maintenant + dicoTaille pour taille des prot

--- envir.py
+++ envir.py
@@ -17,12 +17,13 @@ class envir:
     #fin: indice x de fin du vaisseau (y-x=longueur du vaisseau)
     #protPermanentes : liste des prot seront dans le sang a chaque essai et seront affichees
     #protTotales : liste de ttes  les proteines dans le sang
-    #dicoRel : contient pour chaque prot ( avec cb de prot reagit, (affinite,devient),(inhibe), 'c' si forme cplexe 'p' si prot)
+    #dicoRel : contient pour chaque prot ( avec cb de prot reagit, (affinite,devient),(inhibe), 'c' si forme cplexe 'p' si forme prot)
     #dicoProt : dico contient toutes les proteines (avec en cle leur nom en string)
 
-    def __init__(self, taille_trou, position_trou, diametre, debut, fin):
+    def __init__(self, taille_trou, position_trou, diametre, debut, fin, vitesse_max_flux):
     	self.taille_trou = taille_trou
         self.position_trou = position_trou
+        self.vitesse_max_flux = vitesse_max_flux
 
     	self.diametre = diametre
         self.debut=debut
@@ -34,6 +35,7 @@ class envir:
         p1 = protein(20, random.random()*fin, random.random()*fin)
         p2 = protein(40, random.random()*fin, random.random()*fin)
         p3 = protein(30, random.random()*fin, random.random()*fin)
+        p4 = protein(44, random.random()*fin, random.random()*fin)
 
         self.dicoProt={}
 
@@ -41,7 +43,7 @@ class envir:
         self.dicoProt['TF']=[p2]
         self.dicoProt['X']=[]
         #on fait une liste pour les complexes ? ou on met juste attribut active ?
-        self.dicoProt['prothrombine']=[]
+        self.dicoProt['prothrombine']=[p4]
         self.dicoProt['Xa']=[p3]
         self.dicoProt['V']=[]
         self.dicoProt['fibrinogene']=[]
@@ -64,21 +66,23 @@ class envir:
         self.dicoRel['fibrinogene']=(1,'thrombine','fibrine','p')
         self.dicoRel['thrombine']=(1,'fibrinogene','fibrine','p')
 
+        #dictionnaire contient les tailles de chaque type de prot
+        #rempli au hasard pour le moment, a changer apres!!
+        self.dicoTaille={}
+
+        self.dicoTaille['fVIIa']=[10]
+        self.dicoTaille['TF']=[10]
+        self.dicoTaille['X']=[10]
+        self.dicoTaille['prothrombine']=[10]
+        self.dicoTaille['Xa']=[10]
+        self.dicoTaille['V']=[10]
+        self.dicoTaille['fibrinogene']=[10]
+        self.dicoTaille['thrombine']=[10]
+        self.dicoTaille['fibrine']=[10]
+        self.dicoTaille['plaquettes']=[10]
 
 
 
-
-
-        #METTRE LE NOM DES LISTES A LA PLACE DES NOMS DE PROT DANS LE DICO
-        #pas possible, cles peuvent pas etre des types listes.. types prot ?
-
-        #pb=besoin des prot pour appeler les fonctions, donc dans le dico je dois avoir des prot... 
-
-        #solution = un dico ou un type de facteur en cle et toutes les prot en valeur
-        #du coup : plus besoin de liste des prot totales, on parcourt tout le dico
-        #pr chaque prot d'un type, on fait dicoRel[cle][1] detection
-
-        #et si cle=string et valeurs = les listes? comment on a la string ?
 
         self.venin=""
         self.vitesse_lim=20
@@ -92,46 +96,62 @@ class envir:
         for typeProt,l in self.dicoProt.items(): #on parcourt chaque liste de prot (l=liste d'un type de prot)
 
             for i in xrange(len(l)): #on regarde chaque prot de cette liste  (l[i]=une prot de cette liste d'un type)
+                print "l[i]",l[i]
 
                 if l[i].activation==False: #si est pas deja activee (auquel cas ne peut pas bouger)
+                    move = True #de base peut bouger, sauf si rencontre une prot ac qui peut reagir
+                    prot = 0 #proteine avec qui va reagir 
 
                     if self.dicoRel[typeProt][0]==1: #si peut reagir qu'avec un type de proteine
-                        move=True  #de base peut bouger, sauf si rencontre une prot ac qui peut reagir
-
                         for j in self.dicoProt[self.dicoRel[typeProt][1]]: #pour chaque prot de liste des prot avec qui peut reagir (j=type prot)
                             if l[i].detection(j)== True:  #si detecte une des prot avec qui peut reagir
                                 move = False
+                                prot=j
 
                         if move == True: #si va bouger, a rencontre aucune prot ac qui peut reagir (regarder apres fin boucle for)
 
-                            l[i].move(self.dt, self.vitesse_lim, self.position_trou, self.taille_trou, self.debut, self.fin, self.diametre)
+                            l[i].move(self.dt, self.vitesse_lim, self.position_trou, self.taille_trou, self.debut, self.fin, self.diametre,self.vitesse_max_flux)
 
-                            #+ les faire se transformer !
+                        if move == False: #si a rencontre une prot donc va reagir
+                            self.reaction(typeProt,l[i],prot)  
+
+
                     if self.dicoRel[typeProt][0]>1: #si peut reagir avec 2 types de prot
-                        move=True
-
                         for reactif in self.dicoRel[typeProt][1]: #pour chacun des reactifs ac qui peut reagir
                             for j in self.dicoProt[reactif]:
                                 if l[i].detection(j)==True:
                                     move=False
+                                    prot=j
                         if move == True:
-                            l[i].move(self.dt, self.vitesse_lim, self.position_trou, self.taille_trou, self.debut, self.fin, self.diametre)
+                            l[i].move(self.dt, self.vitesse_lim, self.position_trou, self.taille_trou, self.debut, self.fin, self.diametre, self.vitesse_max_flux)
+                        
+                        if move == False:
+                            self.reaction(typeProt,l[i],prot)
 
+
+
+    #si rencontre pls prot ac qui peut reagir !!
 
     #----------------------------------------------------------------------------------------------------
     #                                      reaction
     #----------------------------------------------------------------------------------------------------
-    def reaction(self,typeProt,prot):
-        if self.dicoRel[typeProt][3]=='p':
-            self.dicoProt[typeProt].remove(prot) #on enleve la proteine qui se transforme du tableau
+    #cree les nouvelles prot ou complexes
+    #enleve les proteines qui ont reagit de leur liste de prot
+    #typeProt=type d'une des prot qui reagit (en string)
+    #prot et prot 2 = objets proteines qui reagissent
 
+    #rq : finalement pas de disctinction entre complexe et proteine car posait pb d'avoir un tuple de 2 prot et non une prot dans la liste des 
+    #proteines complexees (notamment pour prot.x, un tuple a pas d'attribut x)
+    #il faudra, dans la methode d'affichage, faire 2 boules quand c'est un complexe
 
+    def reaction(self,typeProt,prot,prot2):
+        #on cree la nouvelle proteine qui aura comme cord la moyenne des coords des 2 autres prot
+        p = protein(self.dicoTaille[self.dicoRel[typeProt][1]], (prot.x+prot2.x)/2, (prot.y+prot2.y)/2)
 
-
-
+        self.dicoProt[typeProt].remove(prot) #on enleve du tableau la proteine qui se transforme 
+        self.dicoProt[self.dicoRel[typeProt][1]].remove(prot2)  #on enleve l'autre prot qui reagit du tableau
 
             
-
 
 
     #----------------------------------------------------------------------------------------------------
@@ -143,8 +163,8 @@ class envir:
 
 
 
-#taille_trou, position_trou, diametre, debut, fin
-e=envir(20,40,80,0,100)
+#taille_trou, position_trou, diametre, debut, fin,vitesse_max_flux
+e=envir(20,40,80,0,100,10)
 # #dico normaux 
 # print e.dicoRel['V']
 # print e.dicoRel['V'][0]   #donne la proteine avec qui reagit
@@ -165,4 +185,8 @@ print 'fin : fVIIa',e.dicoProt['fVIIa']
 print 'fin : TF',e.dicoProt['TF']
 print 'fin : Xa',e.dicoProt['Xa']
 
-print e.dicoRel['TF'][3]
+#reaction(self,typeProt,prot,prot2)
+#e.reaction('fVIIa',e.dicoProt['fVIIa'][0],e.dicoProt['TF'][0])
+print e.dicoProt['fVIIa']
+print e.dicoProt['TF']
+print e.dicoProt[e.dicoRel['TF'][1]]
