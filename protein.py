1601bf728e0129b4f813e562ebbf110430e932cf
Duran Audrey <aduran@bs406-s16-22.insa-lyon.fr>
2015-06-01 11:30:26 +0200

    Enleve un if active dans methode move (car repete dans envir)

--- protein.py
+++ protein.py
@@ -104,53 +104,52 @@ class protein:
 	# vitesse max flux et la vitesse du flux qui part vers la blessure
 	def move(self, dt, vitesse_lim, position_trou, taille_trou, debut, fin, diametre, vitesse_max_flux):
 
-		if self.activation == False :
 
-			self.x += dt * random.uniform(-1/16*vitesse_lim, vitesse_lim)
-			self.y += dt * random.uniform(-vitesse_lim, vitesse_lim)
+		self.x += dt * random.uniform(-1/16*vitesse_lim, vitesse_lim)
+		self.y += dt * random.uniform(-vitesse_lim, vitesse_lim)
 
 
-			# Pour eviter qu elle sorte du vaiseau par le haut
-			if self.y < 0 :
-				self.y = 0.
+		# Pour eviter qu elle sorte du vaiseau par le haut
+		if self.y < 0 :
+			self.y = 0.
 
-			# Pour eviter qu elle sorte par le cote gauche
-			if self.x < 0:
-				self.x = 0.
+		# Pour eviter qu elle sorte par le cote gauche
+		if self.x < 0:
+			self.x = 0.
 
-			# Pour eviter qu elle sorte du vaiseau tant qu il n y a pas le trou
-			if self.y > diametre:
-				if self.x < position_trou:
-					if self.x > position_trou+taille_trou:
-						self.y = diametre
+		# Pour eviter qu elle sorte du vaiseau tant qu il n y a pas le trou
+		if self.y > diametre:
+			if self.x < position_trou:
+				if self.x > position_trou+taille_trou:
+					self.y = diametre
 
-			# Quand elle arrivent a la fin
-			if self.x > fin :
+		# Quand elle arrivent a la fin
+		if self.x > fin :
+			self.x = debut
+			self.y = random.uniform(0, diametre)
+
+
+
+		# Quand elle sort par le trou, on considere a partir d un moment que elle ne soit plus en contact avec le reste lorsqu
+		# elle sort d un certain perimetre ici on aurait un cercle de rayon 150
+		if self.y > diametre:
+			if math.sqrt((self.x - position_trou+taille_trou/2)**2 + (self.y - diametre)**2) > math.sqrt(150):
 				self.x = debut
 				self.y = random.uniform(0, diametre)
 
 
-
-			# Quand elle sort par le trou, on considere a partir d un moment que elle ne soit plus en contact avec le reste lorsqu
-			# elle sort d un certain perimetre ici on aurait un cercle de rayon 150
-			if self.y > diametre:
-				if math.sqrt((self.x - position_trou+taille_trou/2)**2 + (self.y - diametre)**2) > math.sqrt(150):
-					self.x = debut
-					self.y = random.uniform(0, diametre)
+				# Pour le comptage des proteine qui sont sortie du cercle
+				return 1
 
 
-					# Pour le comptage des proteine qui sont sortie du cercle
-					return 1
+		# pour la proteine qui part dans le flux secondaire
+		if self.x > position_trou - taille_trou:
+			if self.x < position_trou + taille_trou:
+				self.attraction(dt, position_trou, taille_trou, vitesse_max_flux, diametre)
 
 
-			# pour la proteine qui part dans le flux secondaire
-			if self.x > position_trou - taille_trou:
-				if self.x < position_trou + taille_trou:
-					self.attraction(dt, position_trou, taille_trou, vitesse_max_flux, diametre)
-
-
-			# Pour le comptage des proteine qui ne sont pas sortie du cercle
-			return 0
+		# Pour le comptage des proteine qui ne sont pas sortie du cercle
+		return 0
 
 
 
