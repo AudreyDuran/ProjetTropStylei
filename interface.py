fc46d2ee6ec178ac341d1946e26ff4b1614deff3
valejop <thiago.valejo-gomes-pereira@insa-lyon.fr>
2015-06-09 12:32:20 +0200

    interface wxpython post-fermant

--- interface.py
+++ interface.py
@@ -1,113 +1,45 @@
 import wx
-import math
-import numpy as np
-import random
+import os
 
-#commentaire ou truc de merde
-class MyFrame(wx.Frame):
-    def __init__(self, parent, ID, title,pos,size):
-        wx.Frame.__init__(self, parent, ID, title,pos,size)
-        self.SetAutoLayout (True)
+class GUI(wx.Frame):
+	def __init__(self, parent, id):
+		wx.Frame.__init__(self, parent, id,"Venin" , size=(250, 170))
+#		self.panel = wx.Panel(self, -1)
+		self.cb = wx.CheckBox(self, -1, 'Venin', (10, 10))
+		wx.EVT_CHECKBOX(self, self.cb.GetId(), self.Options)
+		self.Run=wx.Button(self,1,"Run",(100,10))
+		self.Bind(wx.EVT_BUTTON, self.OnRun, self.Run)
+		self.Show()
+		self.Centre()
+		self.v=0
 
+	def Options(self, event):
+		if self.cb.GetValue():
+			self.V1=wx.CheckBox(self,-1,'Venin1',(30,50))
+			self.V2=wx.CheckBox(self,-1,'Venin2',(30,70))
+			self.V3=wx.CheckBox(self,-1,'Venin3',(30,90))
+			self.V4=wx.CheckBox(self,-1,'Venin4',(30,110))
 
+		if not self.cb.GetValue(): 
+			self.V1.Show(False)
+			self.V2.Show(False)
+			self.V3.Show(False)
+			self.V4.Show(False)
+			if self.txt:
+				self.txt.Show(False)
 
-        self.cb = wx.CheckBox(self,-1,'venin',(200,200))
-        self.Bind(wx.EVT_CHECKBOX, self.imprimer_b1 , self.cb)
-        
-        tx = wx.TextCtrl( self, -1, 'text', (100,0))
-        self.Bind(wx.EVT_CHECKBOX, self.imprimer_b1 , self.cb)
+	def OnRun(self,event):
+		if self.cb.GetValue():
+			if sum((self.V1.GetValue(),self.V2.GetValue(),self.V3.GetValue(),self.V4.GetValue()))>1:
+				self.txt=wx.StaticText(self,-1, "Choisissez juste un venin",(50,140))
+			else:
+				self.v=(self.V1.GetValue(),self.V2.GetValue(),self.V3.GetValue(),self.V4.GetValue()).index(1)+1
+				os.system("python "+"simulation.py %d"%self.v)
+				self.Close()
+		else:
+			os.system("python "+"simulation.py 0")
+			self.Close()
 
-        self.boutton = []
-        self.compteur = 0
-        self.compteur1 = 0
-        
-        
-        
-    def bouton(self, label):
-        for i,v in enumerate(label):
-        	b = wx.Button(self, i, (0, i*35), (80, 33))
-        	self.boutton.append(b)
-
-
-
-    def bind_bouton(self, fonction):
-    	for i,v in enumerate(self.boutton):
-    		self.Bind(wx.EVT_BUTTON, fonction[i], v)
-
-
-    def imprimer_b0(self,event):
-    	print self.boutton[0].GetLabel()
-
-    def imprimer_b1(self,event):
-    	print self.boutton[1].GetLabel()
-    	self.Destroy()
-
-    def imprimer_b2(self,event):
-    	self.compteur += 1
-    	print self.compteur
-
-    def imprimer_b3(self,event):
-    	self.compteur1 += 1
-    	print self.boutton[3].GetLabel()
-    	if self.compteur1 >= 5:
-    		self.Destroy()
-
-
-
-
-
-    # Bouton
-	def bouton(self, label):
-		for i,v in enumerate(label):
-			b = wx.Button(self, i, v, (0, i*35), (80, 33))
-			self.boutton.append(b)
-
-	def bind_bouton(self, fonction):
-		for i,v in enumerate(self.boutton):
-			self.Bind(wx.EVT_BUTTON, fonction[i], v)
-
-
-
-
-   	# Texte
-	def text(self, label):
-		for i,v in enumerate(label):
-			txt = wx.TextCtrl( self, i, v, (90, i*35))
-			self.texte.append(txt)
-
-	def bind_texte(self, fonction):
-		for i,v in enumerate(self.texte):
-			self.Bind(wx.EVT_TEXT_ENTER, fonction[i], v)
-
-
-
-    # fonction
-	def imprimer(self,event):
-		self.compteur += 1
-		print self.compteur
-
-
-
-class MyApp(wx.App):
-	def OnInit(self):
-		frame = MyFrame(None, -1, "coagulation",(40,40),(400,400))
-
-		label = ['1', '2', '3', '4']
-	
-        # bouton
-		frame.bouton(label)
-		frame.bind_bouton([frame.imprimer_b0,frame.imprimer_b1,frame.imprimer_b2,frame.imprimer_b3])
-
-
-        # texte
-		frame.text(label)
-		# frame.bind_texte(bind)
-
-		frame.Show(True)
-		self.SetTopWindow(frame)
-		return True
-
-
-app = MyApp(0)
-
+app = wx.App(0)
+GUI(None, -1)
 app.MainLoop()
