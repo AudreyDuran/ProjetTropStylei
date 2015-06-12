ad8dc6551eb5109d45c45a2f638060752b67cc6b
Duran Audrey <aduran@bs406-s31-18.insa-lyon.fr>
2015-06-12 12:42:14 +0200

    Modif TF traitement spécial

--- envir.py
+++ envir.py
@@ -158,56 +158,69 @@ class envir:
 			lmemoire.append([])
 			lmemoire.append([])
 
+			#traitement special des facteurs tissulaires, qui doivent pas sortir par le trou mais y rester
+			if typeProt=='TF' or typeProt=='VIIa-TF':
+				for i in xrange(len(l)): # pour chaque facteur tissulaire
+					if l[i].activation==False: #si pas deja activee
+						if l[i].x>self.position_trou-l[i].rayon and l[i].x < self.position_trou + self.taille_trou + l[i].rayon: #si TF dans intervalle du trou +- le rayon
+							if l[i].y > self.diametre - l[i].rayon - 100: #si proche du trou en y (-10= aleatoire, a modifier apres)
+								#alors va se fixer et rester la
+								l[i].y=self.diametre #sur le trou (on garde le meme x)
+								l[i].activation=True #alors TF active
 
-			for i in xrange(len(l)): #on regarde chaque prot de cette liste  (l[i]=une prot de cette liste d'un type)
-
-
-				if l[i].activation==False: #si est pas deja activee (auquel cas ne peut pas bouger)
-					move = True #de base peut bouger, sauf si rencontre une prot ac qui peut reagir
-					prot = 0 #proteine avec qui va reagir 
-					distance = self.fin*self.diametre  #distance entre les 2 prot initialisee tres grande
-
-					if self.dicoRel[typeProt][0]==1: #si peut reagir qu'avec un type de proteine
-						for j in self.dicoProt[self.dicoRel[typeProt][1]]: #pour chaque prot de liste des prot avec qui peut reagir (j=type prot)
-							if l[i].detection(j)== True:  #si detecte une des prot avec qui peut reagir
-								if l[i].distance(j)<distance: #pour que reagisse seulement avec la prot la plus proche
-									move = False
-									prot=j
-									distance=l[i].distance(j)
-
-						if move == True: #si va bouger, a rencontre aucune prot ac qui peut reagir (regarder apres fin boucle for)
-
-							l[i].move(self.dt, self.vitesse_lim, self.position_trou, self.taille_trou, self.debut, self.fin, self.diametre,self.vitesse_max_flux)
-
-						if move == False: #si a rencontre une prot donc va reagir
-							l[i].activation=True #on active les 2 prot
-							prot.activation=True
-							#self.reaction(typeProt,l[i],prot)  
-							lmemoire.append((typeProt,l[i],prot))
+					if l[i].activation== False: #si tjrs pas activee
+						l[i].move(self.dt, self.vitesse_lim, self.position_trou, self.taille_trou, self.debut, self.fin, self.diametre,self.vitesse_max_flux)
 
 
-					if self.dicoRel[typeProt][0]>1: #si peut reagir avec 2 types de prot
-						for reactif in self.dicoRel[typeProt][1]: #pour chacun des reactifs ac qui peut reagir
-							for j in self.dicoProt[reactif]:
-								if l[i].detection(j)==True:
-									if l[i].distance(j)<distance:
-										move=False
+			else: #si pas un facteur tissulaire
+				for i in xrange(len(l)): #on regarde chaque prot de cette liste  (l[i]=une prot de cette liste d'un type)
+
+					if l[i].activation==False: #si est pas deja activee (auquel cas ne peut pas bouger)
+						move = True #de base peut bouger, sauf si rencontre une prot ac qui peut reagir
+						prot = 0 #proteine avec qui va reagir 
+						distance = self.fin*self.diametre  #distance entre les 2 prot initialisee tres grande
+
+						if self.dicoRel[typeProt][0]==1: #si peut reagir qu'avec un type de proteine
+							for j in self.dicoProt[self.dicoRel[typeProt][1]]: #pour chaque prot de liste des prot avec qui peut reagir (j=type prot)
+								if l[i].detection(j)== True:  #si detecte une des prot avec qui peut reagir
+									if l[i].distance(j)<distance: #pour que reagisse seulement avec la prot la plus proche
+										move = False
 										prot=j
 										distance=l[i].distance(j)
-						if move == True:
-							l[i].move(self.dt, self.vitesse_lim, self.position_trou, self.taille_trou, self.debut, self.fin, self.diametre, self.vitesse_max_flux)
-						
-						if move == False:
-							l[i].activation=True #on active les 2 prot
-							prot.activation=True
-							lmemoire[0].append(typeProt)
-							lmemoire[1].append(l[i])
-							lmemoire[2].append(prot)
+
+							if move == True: #si va bouger, a rencontre aucune prot ac qui peut reagir (regarder apres fin boucle for)
+
+								l[i].move(self.dt, self.vitesse_lim, self.position_trou, self.taille_trou, self.debut, self.fin, self.diametre,self.vitesse_max_flux)
+
+							if move == False: #si a rencontre une prot donc va reagir
+								l[i].activation=True #on active les 2 prot
+								prot.activation=True
+								#self.reaction(typeProt,l[i],prot)  
+								lmemoire.append((typeProt,l[i],prot))
 
 
-							#self.reaction(typeProt,l[i],prot)
+						if self.dicoRel[typeProt][0]>1: #si peut reagir avec 2 types de prot
+							for reactif in self.dicoRel[typeProt][1]: #pour chacun des reactifs ac qui peut reagir
+								for j in self.dicoProt[reactif]:
+									if l[i].detection(j)==True:
+										if l[i].distance(j)<distance:
+											move=False
+											prot=j
+											distance=l[i].distance(j)
+							if move == True:
+								l[i].move(self.dt, self.vitesse_lim, self.position_trou, self.taille_trou, self.debut, self.fin, self.diametre, self.vitesse_max_flux)
+							
+							if move == False:
+								l[i].activation=True #on active les 2 prot
+								prot.activation=True
+								lmemoire[0].append(typeProt)
+								lmemoire[1].append(l[i])
+								lmemoire[2].append(prot)
 
-			map(self.reaction,lmemoire[0],lmemoire[1],lmemoire[2])
+
+								#self.reaction(typeProt,l[i],prot)
+
+				map(self.reaction,lmemoire[0],lmemoire[1],lmemoire[2])
 
 
 
